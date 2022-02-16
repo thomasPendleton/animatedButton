b352ceb87a24dc0263f8f68ffeeb19e9b71ca476
Thomas Pendleton <thomas6576@yahoo.com>
2022-02-16 15:17:37 -0800

    started button project, added button and css

---
+++ script.js
@@ -0,0 +1,10 @@
+const buttons = document.querySelectorAll('.ripple')
+
+
+buttons.forEach(button => {
+    button.addEventListener('click', (e) => {
+         const x = e.clientX
+         const y = e.clientY
+         console.log(x, y);
+    })
+})
